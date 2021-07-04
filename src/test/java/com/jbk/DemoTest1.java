80571eb0381f5844dae79c840e7cbd0fbdbc5b9a
prachinimje <prachinimje1995@gmail.com>
2021-07-04 20:11:25 +0530

    adding new files

--- src/test/java/com/jbk/DemoTest1.java
+++ src/test/java/com/jbk/DemoTest1.java
@@ -1,12 +1,15 @@
 package com.jbk;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class DemoTest1 {
+	static WebDriver driver;
 	
 	@Test
 	public void test01(){
 		System.out.println("Hii....");
+		
 	}
 
 }
